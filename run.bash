0c9905821dece46ff2a663d6991a70c83f201d82
regnidorhcS <nikos.agelides@yahoo.com>
2018-04-13 10:10:26 +0100

    correctiion in angle

--- run.bash
+++ run.bash
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 python resetToZero.py
 sleep 5
-python testScript.py
+python resetToZero.py
+python resetToZero.py
 sleep 5
-while true; do
-   python findCygnus.py
-   sleep 300
-done
+python resetToZero.py
+
+#python testScript.py
+#sleep 5
+#while true; do
+#   python findCygnus.py
+#   sleep 300
+#done
 
 #sudo shutdown -h now
