ff118460fcd875501e4cd7cb477bc8da14a8cd32
Regnidorhcs <angelidesnicolas@gmail.com>
2018-06-14 17:00:14 +0000

    Should run now

--- run.bash
+++ run.bash
@@ -2,15 +2,14 @@
 python resetToZero.py
 sleep 5
 python resetToZero.py
-python resetToZero.py
 sleep 5
 python resetToZero.py
 
 #python testScript.py
-#sleep 5
-#while true; do
-#   python findCygnus.py
-#   sleep 300
-#done
+sleep 5
+while true; do
+   python findCygnus.py
+   sleep 300
+done
 
 #sudo shutdown -h now
